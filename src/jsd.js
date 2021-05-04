b0836a8ec33181867125edf5860295bbafd75512
SamGarlick <samgarlick@hotmail.co.uk>
2021-05-04 22:25:41 +0100

    Input event handlers

--- src/jsd.js
+++ src/jsd.js
@@ -1,27 +1,40 @@
 let jIDs = 0
 let jsdElements = {}
 
+// Factory methods
+function JVBox(text) {return new JVBoxClass(text);}
+function JHBox(text) {return new JHBoxClass(text);}
+function JButton(text) {return new JButtonClass(text);}
+function JForm(text) {return new JFormClass(text);}
+function JImage(text) {return new JImageClass(text);}
+function JText(text) {return new JTextClass(text);}
+function JTextbox(text) {return new JTextboxClass(text);}
 
-function jPress(jID) {
-    jsdElements[jID].performPress()
-}
+// Event methods
+// Input Events
+function jsdOnBlur(jID) {jsdElements[jID].performOnBlur();}
+function jsdOnChange(jID) {jsdElements[jID].performOnChange();}
+function jsdOnFocus(jID) {jsdElements[jID].performOnFocus();}
+function jsdOnSelect(jID) {jsdElements[jID].performOnSelect();}
+function jsdOnSubmit(jID) {jsdElements[jID].performOnSubmit();}
+function jsdOnReset(jID) {jsdElements[jID].performOnReset();}
+function jsdOnKeyDown(jID) {jsdElements[jID].performOnKeyDown();}
+function jsdOnKeyPress(jID) {jsdElements[jID].performOnKeyPress();}
+function jsdOnKeyUp(jID) {jsdElements[jID].performOnKeyUp();}
+function jsdOnKeyUp(jID) {jsdElements[jID].performOnKeyUp();}
 
-function jsdOnchange(jID) {
-    jsdElements[jID].performOnChange()
-}
-
-
-class JSDObject {
-    constructor() {
-        this._id = jIDs;
-        jIDs += 1
-         
-        jsdElements[this.id()] = this;
+class JSDStyleClass {
+    constructor(selector) {
+        if (selector == null) {
+            selector = ""
+        }
+        
+        this.selector = selector
         
         this.styles = {
             "css": null,
             "background": null,
-            "border-size": null,
+            "border-width": null,
             "border-color": null,
             "border-style": null,
             "border-radius": null,
@@ -32,6 +45,52 @@ class JSDObject {
             "color": null,
             "font-weight": null
         }
+    }
+    
+    build(objectID) {
+        let styleCode = '';
+        let self = this;
+        Object.keys(self.styles).forEach(function(key) {
+            let value = self.styles[key];
+            if (value != null) {
+                if (key == "css") {
+                    styleCode += value + ";";
+                } else {
+                    styleCode += `${key}:${value};` 
+                }
+            }
+        });
+    
+        if (styleCode.length == 0) {
+            return ""
+        }
+        return `#${objectID}${this.selector}{${styleCode}}`
+    }
+}
+
+
+class JSDObject {
+    constructor() {
+        this._id = jIDs;
+        jIDs += 1
+         
+        jsdElements[this.id()] = this;
+        
+        this.styles = new JSDStyleClass();
+        this.hoverStyles = new JSDStyleClass(":hover");
+        this.activeStyles = new JSDStyleClass(":active");
+        this.focusedStyles = new JSDStyleClass(":focused");
+        
+        this._onBlur = null
+        this._onChange = null
+        this._onFocus = null
+        this._onSelect = null
+        this._onSubmit = null
+        this._onReset = null
+        this._onKeyDown = null
+        this._onKeyPress = null
+        this._onKeyUp = null
+        this._onKeyUp = null
     }
     
     id() {
@@ -50,95 +109,176 @@ class JSDObject {
         return document.getElementById(this.id())
     }
     
-    style() {
-        let styleCode = '';
-        let self = this;
-        Object.keys(self.styles).forEach(function(key) {
-            let value = self.styles[key];
-            if (value != null) {
-                if (key == "css") {
-                    styleCode += value + ";";
-                } else {
-                    styleCode += `${key}:${value};` 
-                }
-            }
-        });
+    getStyleSheet(selector) {
+        if (selector == "hover") {
+            return this.hoverStyles;
+        }
+        if (selector == "active") {
+            return this.activeStyles;
+        }
+        if (selector == "focused") {
+            return this.focusedStyles;
+        }
+        
+        return this.styles;
+    }
     
-        return "style='" + styleCode + "'"
+    style() {
+        let id = this.id()
+        return `<style>${this.styles.build(id)}${this.hoverStyles.build(id)}${this.activeStyles.build(id)}${this.focusedStyles.build(id)}</style>`
     }
     
     setStyle(referenceObject) {
         let self = this;
-        Object.keys(referenceObject.styles).forEach(function(key) {
-            let value = referenceObject.styles[key];
+        Object.keys(referenceObject.styles.styles).forEach(function(key) {
+            let value = referenceObject.styles.styles[key];
             if (value != null) {
-                self.styles[key] = value;
+                self.styles.styles[key] = value;
+            }
+        });
+        Object.keys(referenceObject.hoverStyles.styles).forEach(function(key) {
+            let value = referenceObject.hoverStyles.styles[key];
+            if (value != null) {
+                self.hoverStyles.styles[key] = value;
+            }
+        });
+        Object.keys(referenceObject.activeStyles.styles).forEach(function(key) {
+            let value = referenceObject.activeStyles.styles[key];
+            if (value != null) {
+                self.activeStyles.styles[key] = value;
+            }
+        });
+        Object.keys(referenceObject.focusedStyles.styles).forEach(function(key) {
+            let value = referenceObject.focusedStyles.styles[key];
+            if (value != null) {
+                self.focusedStyles.styles[key] = value;
             }
         });
         
         return this
     }
     
-    css(_style) {
-        this.styles["css"] = _style;
+    css(_style, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["css"] = _style;
         return this
     }
     
-    background(color) {
-        this.styles["background"] = color;
+    background(color, selector) {        
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["background"] = color;
+        
         let elem = this.getElement()
         if (elem != null) {elem.style.background = color}
         return this
     }
     
-    borderSize(size) {
-        this.styles["border-size"] = size;
+    borderSize(size, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["border-width"] = size;
+        
         return this
     }
     
-    borderColor(color) {
-        this.styles["border-color"] = color;
+    borderColor(color, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.tyles["border-color"] = color;
+        
         return this
     }
     
-    borderStyle(style) {
-        this.styles["border-style"] = style;
+    borderStyle(style, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["border-style"] = style;
+        
         return this
     }
     
-    borderRadius(radius) {
-        this.styles["border-radius"] = radius;
+    borderRadius(radius, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["border-radius"] = radius;
+        
         return this
     }
     
-    padding(size) {
-        this.styles["padding"] = size;
+    padding(size, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["padding"] = size;
+        
         return this
     }
     
-    margin(size) {
-        this.styles["margin"] = size;
+    margin(size, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["margin"] = size;
+        
         return this
     }
     
-    font(family) {
-        this.styles["font-family"] = family;
+    font(family, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["font-family"] = family;
         return this
     }
     
-    fontSize(size) {
-        this.styles["font-size"] = size;
+    fontSize(size, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["font-size"] = size;
+        
         return this
     }
     
-    fontColor(color) {
-        this.styles["color"] = color;
+    fontColor(color, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["color"] = color;
+        
         return this
     }
     
-    fontWeight(weight) {
-        this.styles["font-weight"] = weight;
+    fontWeight(weight, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["font-weight"] = weight;
+        
         return this
+    }
+    
+    onBlur(callback) {this._onBlur = callback; return this;}
+    onChange(callback) {this._onChange = callback; return this;}
+    onFocus(callback) {this._onFocus = callback; return this;}
+    onSelect(callback) {this._onSelect = callback; return this;}
+    onSubmit(callback) {this._onSubmit = callback; return this;}
+    onReset(callback) {this._onReset = callback; return this;}
+    onKeyDown(callback) {this._onKeyDown = callback; return this;}
+    onKeyPress(callback) {this._onKeyPress = callback; return this;}
+    onKeyUp(callback) {this._onKeyUp = callback; return this;}
+    onKeyUp(callback) {this._onKeyUp = callback; return this;}
+    
+    // TODO have check for if can run
+    performOnBlur() {this._onBlur(this)}
+    performOnChange() {this._onChange(this)}
+    performOnFocus() {this._onFocus(this)}
+    performOnSelect() {this._onSelect(this)}
+    performOnSubmit() {this._onSubmit(this)}
+    performOnReset() {this._onReset(this)}
+    performOnKeyDown() {this._onKeyDown(this)}
+    performOnKeyPress() {this._onKeyPress(this)}
+    performOnKeyUp() {this._onKeyUp(this)}
+    performOnKeyUp() {this._onKeyUp(this)}
+    
+    eventHandlerHTML() {
+        let id = this.id()
+        let code = "";
+        if (this._onBlur != null) {code += `onblur='jsdOnBlur("${id}")'`}
+        if (this._onChange != null) {code += `onchange='jsdOnChange("${id}")'`}
+        if (this._onFocus != null) {code += `onfocus='jsdOnFocus("${id}")'`}
+        if (this._onSelect != null) {code += `onselect='jsdOnSelect("${id}")'`}
+        if (this._onSubmit != null) {code += `onsubmit='jsdOnSubmit("${id}")'`}
+        if (this._onReset != null) {code += `onreset='jsdOnReset("${id}")'`}
+        if (this._onKeyDown != null) {code += `onkeydown='jsdOnKeyDown("${id}")'`}
+        if (this._onKeyPress != null) {code += `onkeypress='jsdOnKeyPress("${id}")'`}
+        if (this._onKeyUp != null) {code += `onkeyup='jsdOnKeyUp("${id}")'`}
+        if (this._onKeyUp != null) {code += `onkeyup='jsdOnKeyUp("${id}")'`}
+        return code;
     }
 }
 
@@ -173,7 +313,7 @@ class JVBoxClass extends JSDObject {
     }
     
     build () {
-        let code = "<table "+this.style()+" border=0 cellpadding=0 cellspacing=0>"
+        let code = this.style() + "<table border=0 cellpadding=0 cellspacing=0>"
         for (let e = 0; e < this.elements.length; e++) {
             if (e > 0) {
                 code += "<tr><td height='"+ this._spacing +"px'></td></tr>"
@@ -182,11 +322,6 @@ class JVBoxClass extends JSDObject {
         }
         return code + "</table>";
     }
-}
-
-
-function JVBox(text) {
-    return new JVBoxClass(text)
 }
 
 
@@ -204,7 +339,7 @@ class JHBoxClass extends JSDObject {
     }
     
     build () {
-        let code = "<table "+this.style()+" border=0 cellpadding=0 cellspacing=0>"
+        let code = this.style() + "<table id='"+this.id()+"' border=0 cellpadding=0 cellspacing=0>"
         for (let e = 0; e < this.elements.length; e++) {
             if (e > 0) {
                 code += "<td width='"+ this._spacing +"px'></td>"
@@ -216,17 +351,21 @@ class JHBoxClass extends JSDObject {
 }
 
 
-function JHBox(text) {
-    return new JHBoxClass(text)
-}
-
-
 class JButtonClass extends JSDObject {
     constructor(text) {
         super()
         
         this.text = text;
         this._press = null;
+        
+        this.borderSize("0px")
+        this.borderRadius("2px")
+        this.padding("8px 16px")
+        this.background("#eeeeee")
+        this.font("arial")
+        
+        this.background("#dddddd", "hover")
+        this.background("#cccccc", "active")
     }
     
     press(action) {
@@ -243,16 +382,9 @@ class JButtonClass extends JSDObject {
         if (typeof(text) != "string") {
             text = this.text.build()
         }
-        return `<button type='button' id='${this.id()}' ${this.style()} onclick="jPress('${this.id()}')" ontap="dPress('${this.id()}')">${this.text}</button>`
+        return `${this.style()}<button class='jsd-button' type='button' id='${this.id()}' onclick="jPress('${this.id()}')" ontap="dPress('${this.id()}')">${this.text}</button>`
     }
 }
-
-
-function JButton(text) {
-    return new JButtonClass(text)
-}
-
-
 
 
 class JFormClass extends JSDObject {
@@ -290,11 +422,6 @@ class JFormClass extends JSDObject {
 }
 
 
-function JForm(text) {
-    return new JFormClass(text)
-}
-
-
 
 class JImageClass extends JSDObject {
     constructor() {
@@ -311,11 +438,6 @@ class JImageClass extends JSDObject {
     build() {
         return "<img src='" + this._src + "'/>"
     }
-}
-
-
-function JImage(text) {
-    return new JImageClass(text)
 }
 
 
@@ -362,14 +484,10 @@ class JTextClass extends JSDObject {
     }
     
     build() {
-        return `<span id='${this.id()}' ${this.style()}>${this._text}</span>`
+        return `${this.style()}<span id='${this.id()}'>${this._text}</span>`
     }
 }
 
-
-function JText(text) {
-    return new JTextClass(text)
-}
 
 
 
@@ -382,6 +500,15 @@ class JTextboxClass extends JSDObject {
         
         this._placeholder = placeholder;
         this._value = ""
+        
+        this.borderSize("0px")
+        this.borderRadius("2px")
+        this.padding("8px 16px")
+        this.background("#eeeeee")
+        this.font("arial")
+        this.background("#dddddd", "hover")
+        this.background("#cccccc", "active")
+        this.background("#bbbbbb", "focus")
     }
     
     value(text) {
@@ -409,12 +536,19 @@ class JTextboxClass extends JSDObject {
         }
     }
     
+    // TODO Override to update value
+    performOnBlur() {this._onBlur(this)}
+    performOnChange() {this._onChange(this)}
+    performOnFocus() {this._onFocus(this)}
+    performOnSelect() {this._onSelect(this)}
+    performOnSubmit() {this._onSubmit(this)}
+    performOnReset() {this._onReset(this)}
+    performOnKeyDown() {this._onKeyDown(this)}
+    performOnKeyPress() {this._onKeyPress(this)}
+    performOnKeyUp() {this._onKeyUp(this)}
+    performOnKeyUp() {this._onKeyUp(this)}
+    
     build() {
-        return `<input id='${this.id()}' type="text" ${this.style()} placeholder="${this._placeholder}" value="${this._value}" onchange='jsdOnchange("${this.id()}")'/>`
+        return `${this.style()}<input id='${this.id()}' class='jsd-textbox' type="text" placeholder="${this._placeholder}" value="${this._value}" ${this.eventHandlerHTML()}/>`
     }
-}
-
-
-function JTextbox(text) {
-    return new JTextboxClass(text)
 }
