f73f7360c3b1d289e8efe73ba8d7c36612a5d1e9
SamGarlick <samgarlick@hotmail.co.uk>
2021-05-05 19:20:34 +0100

    Attribute system

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Eclair
-
-
 Don't ask why, ask why not. This library is a declarative UI builder for websites. Angular, React, Vue, etc. are all designed to help make coding web UIs easier (they do a good job of it). But often you'll find yourself still having to use HTML and CSS to do things. And the code isn't much easier to do anyways. This library takes inspiration for SwiftUI's superiour way of coding a UI...but for websites. It's easy, simple and you need to write a lot less code.
 
 ## Examples
 ### Simple Button
-HTML + CSS + JS
+_HTML + CSS + JS_
 ``` html
 <style>
     .myButton {
@@ -20,7 +18,7 @@ HTML + CSS + JS
 <button class="myButton" onclick="myButtonClick()">Hello There!</button>
 ```
 
-Eclair
+_Eclair_
 ``` html
 <script>
 eclair.Button("Hello There!")
@@ -33,7 +31,7 @@ eclair.Button("Hello There!")
 ```
 
 ### Destructive Select
-HTML + CSS + JS
+_HTML + CSS + JS_
 ``` html
 <script>
     function destruct(elem) {
@@ -50,13 +48,12 @@ HTML + CSS + JS
 </select>
 ```
 
-Eclair
+_Eclair_
 ``` html
 <script>
 eclair.Select()
-    .addOptions([
-        "Welcome to the Jungle", "It's so Easy", "Nightrain",
-        "Out Ta Get Me", "Mr. Brownstone", "Sweet Child O' Mine"])
+    .addOptions(["Welcome to the Jungle", "It's so Easy", "Nightrain",
+                 "Out Ta Get Me", "Mr. Brownstone", "Sweet Child O' Mine"])
     .selectedIndex(5)
     .onChange((self) => {
         self.removeOption(self.value())
@@ -71,7 +68,10 @@ eclair.Select()
 ### styling
 ### event handling
 ### Subclasses
-### Other things, 'imports', 'getElement', etc
+### Other things, 'versioning', 'imports', 'getElement', etc
+### Custom elements.
 ...
 
 # More documentation coming one day...
+# Element Specific
+### Selects
--- eclair.js
+++ eclair.js
@@ -1,3 +1,8 @@
+// TODO Add events to callbacks
+// TODO Add update event handlers on change
+// TODO Prevent layered on click events
+// TODO Hide show elements
+
 let eclair = {
     _ids: 0,
     _elements: {},
@@ -10,11 +15,10 @@ let eclair = {
     Form: function(elements) {return new EclairForm(elements);},
     Image: function() {return new EclairImage();},
     Text: function(text) {return new EclairText(text);},
-    Textbox: function(placeholder) {return new EclairTextbox(placeholder);},
+    Textbox: function() {return new EclairTextbox();},
     
     Select: function() {return new EclairSelect();},
     
-    // TODO Add events to callbacks
     // Event methods
     // Input Events
     onBlurCallback: function(eID) {this._elements[eID].performOnBlur();},
@@ -51,20 +55,7 @@ class EclairStyleClass {
         
         this.selector = selector
         
-        this.styles = {
-            "css": null,
-            "background": null,
-            "border-width": null,
-            "border-color": null,
-            "border-style": null,
-            "border-radius": null,
-            "margin": null,
-            "padding": null,
-            "font-family": null,
-            "font-size": null,
-            "color": null,
-            "font-weight": null
-        }
+        this.styles = {}
     }
     
     build(objectID) {
@@ -92,6 +83,8 @@ class EclairStyleClass {
 
 class EclairObject {
     constructor() {
+        this.attributes = {}
+        
         this._id = eclair.newID();
         eclair._elements[this.id()] = this;
         
@@ -139,11 +132,31 @@ class EclairObject {
         document.getElementById(elemID).innerHTML = this.build();
     }
     
-    getElement() {
-        return document.getElementById(this.id())
+    getElement(callback) {
+        let elem = document.getElementById(this.id());
+        if (callback != null && elem != null) {
+            callback(elem)
+        }
+        return elem;
     }
     
     update() {}
+    
+    getAttr(key) {
+        this.getElement(elem => {return elem.getAttribute(key)})
+        return this.attributes[key];
+    }
+    
+    setAttr(key, value) {
+        if (value == null) {
+            delete this.attributes[key];
+            this.getElement(elem => {elem.removeAttribute(key)})
+        } else {
+            this.attributes[key] = value;
+            this.getElement(elem => {elem.setAttribute(key, value)})
+        }
+        return this;
+    }
     
     getStyleSheet(selector) {
         if (selector == "hover") {
@@ -200,12 +213,19 @@ class EclairObject {
         return this
     }
     
+    display(_display, selector) {
+        let stylesheet = this.getStyleSheet(selector)
+        stylesheet.styles["display"] = _display;
+        
+        this.getElement((elem) => {elem.style.display = _display})
+        return this
+    }
+    
     background(color, selector) {        
         let stylesheet = this.getStyleSheet(selector)
         stylesheet.styles["background"] = color;
         
-        let elem = this.getElement()
-        if (elem != null) {elem.style.background = color}
+        this.getElement((elem) => {elem.style.background = color})
         return this
     }
     
@@ -278,26 +298,33 @@ class EclairObject {
         return this
     }
     
-    onBlur(callback) {this._onBlur = callback; return this;}
-    onChange(callback) {this._onChange = callback; return this;}
-    onFocus(callback) {this._onFocus = callback; return this;}
-    onSelect(callback) {this._onSelect = callback; return this;}
-    onSubmit(callback) {this._onSubmit = callback; return this;}
-    onReset(callback) {this._onReset = callback; return this;}
-    onKeyDown(callback) {this._onKeyDown = callback; return this;}
-    onKeyPress(callback) {this._onKeyPress = callback; return this;}
-    onKeyUp(callback) {this._onKeyUp = callback; return this;}
-    onMouseDown(callback) {this._onMouseDown = callback; return this;}
-    onMouseUp(callback) {this._onMouseUp = callback; return this;}
-    onMouseOver(callback) {this._onMouseOver = callback; return this;}
-    onMouseOut(callback) {this._onMouseOut = callback; return this;}
-    onMouseMove(callback) {this._onMouseMove = callback; return this;}
-    onClick(callback) {this._onClick = callback; return this;}
-    onDblClick(callback) {this._onDblClick = callback; return this;}
-    onLoad(callback) {this._onLoad = callback; return this;}
-    onError(callback) {this._onError = callback; return this;}
-    onUnload(callback) {this._onUnload = callback; return this;}
-    onResize(callback) {this._onResize = callback; return this;}
+    _updateCallback(callbackKey, callback) {
+        if (callback == null) {
+            this.setAttr(callbackKey.toLowerCase(), null)
+        } else {
+            this.setAttr(callbackKey.toLowerCase(), `eclair.${callbackKey}Callback("${this.id()}")`)
+        }
+    }
+    onBlur(callback) {this._updateCallback("onBlur", callback); this._onBlur = callback;  return this;}
+    onChange(callback) {this._updateCallback("onChange", callback); this._onChange = callback; return this;}
+    onFocus(callback) {this._updateCallback("onFocus", callback); this._onFocus = callback; return this;}
+    onSelect(callback) {this._updateCallback("onSelect", callback); this._onSelect = callback; return this;}
+    onSubmit(callback) {this._updateCallback("onSubmit", callback); this._onSubmit = callback; return this;}
+    onReset(callback) {this._updateCallback("onReset", callback); this._onReset = callback; return this;}
+    onKeyDown(callback) {this._updateCallback("onKeyDown", callback); this._onKeyDown = callback; return this;}
+    onKeyPress(callback) {this._updateCallback("onKeyPress", callback); this._onKeyPress = callback; return this;}
+    onKeyUp(callback) {this._updateCallback("onKeyUp", callback); this._onKeyUp = callback; return this;}
+    onMouseDown(callback) {this._updateCallback("onMouseDown", callback); this._onMouseDown = callback; return this;}
+    onMouseUp(callback) {this._updateCallback("onMouseUp", callback); this._onMouseUp = callback; return this;}
+    onMouseOver(callback) {this._updateCallback("onMouseOver", callback); this._onMouseOver = callback; return this;}
+    onMouseOut(callback) {this._updateCallback("onMouseOut", callback); this._onMouseOut = callback; return this;}
+    onMouseMove(callback) {this._updateCallback("onMouseMove", callback); this._onMouseMove = callback; return this;}
+    onClick(callback) {this._updateCallback("onClick", callback); this._onClick = callback; return this;}
+    onDblClick(callback) {this._updateCallback("onDblClick", callback); this._onDblClick = callback; return this;}
+    onLoad(callback) {this._updateCallback("onLoad", callback); this._onLoad = callback; return this;}
+    onError(callback) {this._updateCallback("onError", callback); this._onError = callback; return this;}
+    onUnload(callback) {this._updateCallback("onUnload", callback); this._onUnload = callback; return this;}
+    onResize(callback) {this._updateCallback("onResize", callback); this._onResize = callback; return this;}
     
     // TODO have check for if can run
     performOnBlur() {this.update(); this._onBlur(this)}
@@ -321,30 +348,13 @@ class EclairObject {
     performOnUnload() {this.update(); this._onUnload(this)}
     performOnResize() {this.update(); this._onResize(this)}
     
-    eventHandlerHTML() {
-        let id = this.id()
-        let code = "";
-        if (this._onBlur != null) {code += ` onblur='eclair.onBlurCallback("${id}")'`}
-        if (this._onChange != null) {code += ` onchange='eclair.onChangeCallback("${id}")'`}
-        if (this._onFocus != null) {code +=  `onfocus='eclair.onFocusCallback("${id}")'`}
-        if (this._onSelect != null) {code += ` onselect='eclair.onSelectCallback("${id}")'`}
-        if (this._onSubmit != null) {code += ` onsubmit='eclair.onSubmitCallback("${id}")'`}
-        if (this._onReset != null) {code += ` onreset='eclair.onResetCallback("${id}")'`}
-        if (this._onKeyDown != null) {code += ` onkeydown='eclair.onKeyDownCallback("${id}")'`}
-        if (this._onKeyPress != null) {code += ` onkeypress='eclair.onKeyPressCallback("${id}")'`}
-        if (this._onKeyUp != null) {code += ` onkeyup='eclair.onKeyUpCallback("${id}")'`}
-        if (this._onMouseDown != null) {code += ` onmousedown='eclair.onMouseDownCallback("${id}")'`}
-        if (this._onMouseUp != null) {code += ` onmouseup='eclair.onMouseUpCallback("${id}")'`}
-        if (this._onMouseOver != null) {code += ` onmouseover='eclair.onMouseOverCallback("${id}")'`}
-        if (this._onMouseOut != null) {code += ` onmouseout='eclair.onMouseOutCallback("${id}")'`}
-        if (this._onMouseMove != null) {code += ` onmousemove='eclair.onMouseMoveCallback("${id}")'`}
-        if (this._onClick != null) {code += ` onclick='eclair.onClickCallback("${id}")'`}
-        if (this._onDblClick != null) {code += ` ondblclick='eclair.onDblClickCallback("${id}")'`}
-        if (this._onLoad != null) {code += ` onload='eclair.onLoadCallback("${id}")'`}
-        if (this._onError != null) {code += ` onerror='eclair.onErrorCallback("${id}")'`}
-        if (this._onUnload != null) {code += ` onunload='eclair.onUnloadCallback("${id}")'`}
-        if (this._onResize != null) {code += ` onresize='eclair.onResizeCallback("${id}")'`}
-        return code;
+    buildAttributeHTML() {
+        let self = this
+        let attrHTML = ""
+        Object.keys(this.attributes).forEach(function(key) {
+            attrHTML += ` ${key}='${self.attributes[key]}'`
+        });
+        return attrHTML;
     }
 }
 
@@ -417,33 +427,6 @@ class EclairHBox extends EclairObject {
 }
 
 
-class EclairButton extends EclairObject {
-    constructor(text) {
-        super()
-        
-        this.text = text;
-        this._press = null;
-        
-        this.borderSize("0px")
-        this.borderRadius("2px")
-        this.padding("8px 16px")
-        this.background("#eeeeee")
-        this.font("arial")
-        
-        this.background("#dddddd", "hover")
-        this.background("#cccccc", "active")
-    }
-    
-    build() {
-        let text = this.text;
-        if (typeof(text) != "string") {
-            text = this.text.build()
-        }
-        return `${this.style()}<button type='button' id='${this.id()}'  ${this.eventHandlerHTML()}>${this.text}</button>`
-    }
-}
-
-
 class EclairForm extends EclairObject {
     constructor(elements) {
         super()
@@ -480,25 +463,6 @@ class EclairForm extends EclairObject {
 
 
 
-class EclairImage extends EclairObject {
-    constructor() {
-        super()
-        
-        this._src = null
-    }
-    
-    src(_src) {
-        this._src = _src;
-        return this;
-    }
-    
-    build() {
-        return "<img src='" + this._src + "' ${this.eventHandlerHTML()}/>"
-    }
-}
-
-
-
 
 class EclairText extends EclairObject {
     constructor(text) {
@@ -522,18 +486,13 @@ class EclairText extends EclairObject {
             return this._text
         } else {
             this._text = value;
-            
-            let elem = this.getElement()
-            if (elem != null) {
-                elem.innerHTML = this._text
-            }
-            
+            this.getElement((elem) => {elem.innerHTML = value});
             return this
         }
     }
     
     build() {
-        return `${this.style()}<span id='${this.id()}' ${this.eventHandlerHTML()}>${this._text}</span>`
+        return `${this.style()}<span id='${this.id()}' ${this.buildAttributeHTML()}>${this._text}</span>`
     }
 }
 
@@ -542,11 +501,9 @@ class EclairText extends EclairObject {
 
 
 class EclairTextbox extends EclairObject {
-    constructor(placeholder) {
+    constructor() {
         super()
-        
-        this._placeholder = placeholder;
-        this._value = ""
+        this.setAttr("type", "text")
         
         this.borderSize("0px")
         this.borderRadius("2px")
@@ -558,31 +515,64 @@ class EclairTextbox extends EclairObject {
         this.background("#bbbbbb", "focused")
     }
     
-    value(text) {
-        if (text == null) {
-            return this._value
+    name(_name) {
+        if (_name == null) {
+            return this.getAttr("name")
         } else {
-            this._value = text;
-            
-            let elem = this.getElement();
-            if (elem != null) {
-                this.elem = text;
-            }
-            
+            this.setAttr("name", _name)
             return this
         }
     }
     
-    update(func) {
-        let elem = this.getElement();
-        if (elem != null) {
-            this._value = elem.value;
+    value(text) {
+        if (text == null) {
+            return this.getAttr("value")
+        } else {
+            this.setAttr("value", text)
+            return this
         }
     }
     
-    build() {
-        return `${this.style()}<input id='${this.id()}' type="text" placeholder="${this._placeholder}"  value="${this._value}" ${this.eventHandlerHTML()}/>`
+    placeholder(_placeholder) {
+        if (text == null) {
+            return this.getAttr("placeholder")
+        } else {
+            this.setAttr("placeholder", _placeholder)
+            return this
+        }
     }
+    
+    password(isPassword) {
+        if (isPassword == null) {
+            return this.getAttr("type") == "password"
+        } else {
+            this.setAttr("type", isPassword? "password":'text')
+            return this
+        }
+    }
+    
+    maxLength(maxLength) {
+        if (maxLength == null) {
+            return this.getAttr("maxlength");
+        } else {
+            this.setAttr("maxlength", maxLength)
+        }
+        return this
+    } 
+    
+    build() {
+        return `${this.style()}<input id='${this.id()}' ${this.buildAttributeHTML()}/>`
+    }
+    
+//autocomplete	Sets or returns the value of the autocomplete attribute of a text field
+//autofocus	Sets or returns whether a text field should automatically get focus when the page loads
+//disabled	Sets or returns whether the text field is disabled, or not
+//list	Returns a reference to the datalist that contains the text field
+//pattern	Sets or returns the value of the pattern attribute of a text field
+//readOnly	Sets or returns whether a text field is read-only, or not
+//required	Sets or returns whether the text field must be filled out before submitting a form
+//size	Sets or returns the value of the size attribute of a text field
+
 }
 
 
@@ -597,22 +587,14 @@ class EclairTextbox extends EclairObject {
 class EclairSelect extends EclairObject {
     constructor() {
         super()
-        
-        this.name = ""
         this.options = []
     }
     
     name(newName) {
         if (newName == null) {
-            return this.name;
+            return this.getAttr("name");
         } else {
-            this.name = newName;
-
-            let elem = this.getElement();
-            if (elem != null) {
-                elem.setAttribute("name", newName);
-            }
-
+            this.setAttr("name", newName);
             return this;
         }
     }
@@ -624,11 +606,8 @@ class EclairSelect extends EclairObject {
             for (let n = 0; n < this.options.length; n++) {
                 this.options[n].selected = newValue == this.options[n].value;
             }
-
-            let elem = this.getElement();
-            if (elem != null) {
-                this.getElement().value = newValue;
-            }
+            
+            this.getElement(elem => {elem.value = newValue})
 
             return this;
         }
@@ -639,14 +618,10 @@ class EclairSelect extends EclairObject {
             return this.getElement().selectedIndex;
         } else {
             for (let n = 0; n < this.options.length; n++) {
-                console.log(index == n)
                 this.options[n].selected = index == n;
             }
+            this.getElement(elem => {elem.selectedIndex = `${index}`})
             
-            let elem = this.getElement();
-            if (elem != null) {
-                this.getElement().selectedIndex = `${index}`;
-            }
             
             return this;
         }
@@ -722,9 +697,73 @@ class EclairSelect extends EclairObject {
             options += this.buildOptionHTML(this.options[n]);
         }
         
-        return `${this.style()}<select id='${this.id()}' name='${this.name}' ${this.eventHandlerHTML()}>${options}</select>`
+        return `${this.style()}<select id='${this.id()}' name='${this.name}' ${this.buildAttributeHTML()}>${options}</select>`
+    }
+}
+
+class EclairButton extends EclairObject {
+    constructor(text) {
+        super()
+        
+        this.text = text;
+        
+        this.borderSize("0px")
+        this.borderRadius("2px")
+        this.padding("8px 16px")
+        this.background("#eeeeee")
+        this.font("arial")
+        
+        this.background("#dddddd", "hover")
+        this.background("#cccccc", "active")
+    }
+    
+    value(newText) {
+        this.text = newText;
+        this.getElement((elem) => {
+            let html = newText;
+            if (typeof(html) != "string") {
+                html = html.build()
+            }
+            elem.innerHTML = html;
+        });
+        return this
+    }
+    
+    build() {
+        let text = this.text;
+        if (typeof(text) != "string") {
+            text = this.text.build()
+        }
+        return `${this.style()}<button type='button' id='${this.id()}'  ${this.buildAttributeHTML()}>${this.text}</button>`
     }
 }
 
 
 
+/*
+    Standard Elements
+*/
+class EclairImage extends EclairObject {
+    constructor() {
+        super()
+        this.display("block")
+    }
+    
+    src(_src) {
+        this.setAttr("src", _src)
+        return this;
+    }
+    
+    altText(_alt) {
+        if (_alt == null) {
+            return this.getAttr("alt");
+        } else {
+            this.setAttr("alt", _alt)
+            return this
+        }
+    }
+    
+    build() {
+        return `<img${this.buildAttributeHTML()}/>`
+    }
+}
